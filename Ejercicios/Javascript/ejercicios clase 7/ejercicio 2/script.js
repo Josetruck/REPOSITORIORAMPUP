bb6152ef6dc3bc669aafe9b661af5cc00bd31f60
josetruck <olemiluis@gmail.com>
2022-09-21 11:26:51 +0200

    Correccion ejercicio 1

--- Ejercicios/Javascript/ejercicios clase 7/ejercicio 2/script.js
+++ Ejercicios/Javascript/ejercicios clase 7/ejercicio 2/script.js
@@ -1,7 +1,11 @@
 // Contador de cifras.
 function contadorcifras() {
     var x = prompt("Introduzca un numero entero")
-    alert(x.length)
+    var count = 0
+    for (let i = 0; i < x - 1; i++) {
+        count +1;
+    }
+alert(count)
 }
 
 // Secuencia de *
@@ -41,12 +45,31 @@ function triangulo() {
 
 // Comparador de fechas
 
-const diasenmes=[0,31,28,31,30,31,30,31,31,30,31,30,31];
-var dia1 = prompt("introduce un dia");
-var mes1 = prompt("introduce un mes");
-var dia2 = prompt("introduce otro dia");
-var mes2 = prompt("introduce otro mes");
-
-function suma1(){
-    
+function comparador() {
+    const diasenmes = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    var dia1 = parseInt(prompt("introduce un dia"));
+    var mes1 = parseInt(prompt("introduce un mes en numero, por ejemplo Enero=1, Febrero=2,..."));
+    var dia2 = parseInt(prompt("introduce otro dia"));
+    var mes2 = parseInt(prompt("introduce otro mes en numero, por ejemplo Enero=1, Febrero=2,..."));
+    var totaldias1 = 0;
+    var totaldias2 = 0;
+    if (mes1 > 12) {
+        alert("los años no tienen " + mes1 + " meses.")
+    }
+    if (mes2 > 12) {
+        alert("los años no tienen " + mes2 + " meses.")
+    }
+    for (let i = 0; i <= mes1; i++) {
+        totaldias1 += diasenmes[i];
+    }
+    for (let i = 0; i <= mes2; i++) {
+        totaldias2 += diasenmes[i]; 2
+    }
+    totaldias1 += dia1;
+    totaldias2 += dia2;
+    diferencia = totaldias1 - totaldias2;  
+    if (diferencia < 0) {
+        diferencia = diferencia * -1;
+    }
+    alert("La diferencia de dias entre fechas es de " + diferencia + " dias");
 }
--- Ejercicios/Javascript/ejercicios clase 7/pruebas/script.js
+++ Ejercicios/Javascript/ejercicios clase 7/pruebas/script.js
@@ -1,4 +1,25 @@
-( function () {
-    let nombre = "Davinia"; //Si la declaráramos con var seguiría siendo local
-    alert("Hola " + nombre);
-})();
+const diasenmes = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+var dia1 = parseInt(prompt("introduce un dia"));
+var mes1 = parseInt(prompt("introduce un mes en numero, por ejemplo Enero=1, Febrero=2,..."));
+var dia2 = parseInt(prompt("introduce otro dia"));
+var mes2 = parseInt(prompt("introduce otro mes en numero, por ejemplo Enero=1, Febrero=2,..."));
+var totaldias1 = 0;
+var totaldias2 = 0;
+
+for(let i=0; i <= mes1; i++){
+    totaldias1 += diasenmes[i];
+}
+for(let i=0; i <= mes2; i++){
+    totaldias2 += diasenmes[i];2
+}
+
+console.log(totaldias1)
+console.log(totaldias2)
+totaldias1 += dia1;
+totaldias2 += dia2;
+diferencia= totaldias1 - totaldias2;
+console.log(diferencia);
+if(diferencia < 0){
+    diferencia = diferencia * -1;
+}
+alert("La diferencia de dias entre fechas es de " + diferencia + " dias");
